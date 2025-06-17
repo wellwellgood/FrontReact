2eb47ad5313b2d2347bea4f425280010301e02c4
WellWellGood <wellwellgood@example.com>
2025-06-17 11:28:44 +0900

    1

--- src/serverF/chatServer/controllers/messageController.js
+++ src/serverF/chatServer/controllers/messageController.js
@@ -44,9 +44,9 @@ exports.saveMessage = async (req, res) => {
     const fileName = file?.originalname?.trim().replace(/\s+/g, "_") || null;
 
     const result = await db.query(
-      `INSERT INTO messages (sender_username, receiver_username, receiver_name, content, fileUrl, file_name, time, file) 
-       VALUES ($1, $2, $3, $4, $5, $6, %7, NOW()) RETURNING *`,
-      [sender_username, receiver_username, receiver_name, content.trim(), fileUrl, fileName, file]
+      `INSERT INTO messages (sender_username, receiver_username, receiver_name, content, fileUrl, file_name, time) 
+       VALUES ($1, $2, $3, $4, $5, $6, NOW()) RETURNING *`,
+      [sender_username, receiver_username, receiver_name, content.trim(), fileUrl, fileName]
     );
 
     const savedMessage = result.rows[0];
@@ -58,7 +58,6 @@ exports.saveMessage = async (req, res) => {
       receiver_name: savedMessage.receiver_name,
       content: savedMessage.content,
       file: file?.filename || null,
-      file,
       file_name: savedMessage.file_name,
       fileUrl: file ? `${req.protocol}://${req.get("host")}/uploads/${file.filename}` : null,
       time: savedMessage.time,

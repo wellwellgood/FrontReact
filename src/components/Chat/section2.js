3e949149789630d83f7081668272858831947cfa
WellWellGood <wellwellgood@example.com>
2025-06-24 13:44:55 +0900

    1

--- src/components/Chat/section2.js
+++ src/components/Chat/section2.js
@@ -32,7 +32,7 @@ const Section2 = () => {
   const [showResults, setShowResults] = useState(false);
   const [showSettings, setShowSettings] = useState(false);
   const [userListError, setUserListError] = useState("");
-  const [handleFileSelect, setHandleFileSelect] = useState(() => (e) => {
+  const [handleFileSelect, setHandleFileSelect] = useState("");
 
   useEffect(() => {
     const u = sessionStorage.getItem("username");

1352903178b9d78b38a9cab165e3fe5cca85a5ac
WellWellGood <wellwellgood@example.com>
2025-06-24 09:40:49 +0900

    1

--- src/serverF/middlewares/cors.js
+++ src/serverF/middlewares/cors.js
@@ -2,7 +2,7 @@
 import cors from "cors";
 
 const allowedOrigins = [
-  "https://kkydashboard.netlify.app/",
+  "https://kkydashboard.netlify.app",
   "http://localhost:3000",
   "http://localhost:4000",
   "https://react-server-wmqa.onrender.com"

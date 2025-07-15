8bf68b3dd4c29bac6516d0cd450edbf89f2dc5ef
WellWellGood <wellwellgood@example.com>
2025-07-15 13:56:58 +0900

    message Processing only one

--- src/serverF/routes/message.js
+++ src/serverF/routes/message.js
@@ -1,16 +1,15 @@
-// routes/message.js (ESM 버전)
 import express from "express";
 import messageController from "../chatServer/controllers/messageController.js";
 
 const router = express.Router();
 
 // 저장
-router.post("/messages", messageController.saveMessage);
+router.post("/", messageController.saveMessage);
 // 조회
-router.get("/messages", messageController.getMessages);
+router.get("/", messageController.getMessages);
 // 읽음 처리 (전체)
-router.post("/messages/read", messageController.markAllAsRead);
+router.post("/read", messageController.markAllAsRead);
 // 개별 읽음 처리
-router.post("/messages/:messageId/read", messageController.markMessageAsRead);
+router.post("/:messageId/read", messageController.markMessageAsRead);
 
 export default router;

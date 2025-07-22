2dffa254569460e5f22c6b894cf3823889d550be
WellWellGood <wellwellgood@example.com>
2025-07-22 15:23:30 +0900

    verity ID

--- src/utill/api.js
+++ src/utill/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-let BASE_URL = process.env.REACT_APP_API || "http://localhost:4000";
+let BASE_URL = process.env.REACT_APP_API || "http://localhost:10000";
 
 if (!BASE_URL) {
   BASE_URL =
@@ -12,7 +12,7 @@ if (!BASE_URL) {
 console.log("✅ 최종 BASE_URL:", BASE_URL);
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://react-server-wmqa.onrender.com/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
 

31254a11b2d8064ccd067ee818445327db7544b2
WellWellGood <wellwellgood@example.com>
2025-07-22 17:13:45 +0900

    verity ID

--- src/util/api.js
+++ src/util/api.js
@@ -2,11 +2,10 @@ import axios from "axios";
 
 let BASE_URL = process.env.REACT_APP_API || "http://localhost:10000";
 
-if (!BASE_URL) {
-  BASE_URL =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:10000"
-      : "https://react-server-wmqa.onrender.com/api";
+if (typeof window !== "undefined" && window.location.hostname === "localhost") {
+  BASE_URL = "http://localhost:10000/api";
+} else {
+  BASE_URL = "https://react-server-wmqa.onrender.com/api"; // ✅ 🔥 /api 붙임
 }
 
 console.log("✅ 최종 BASE_URL:", BASE_URL);

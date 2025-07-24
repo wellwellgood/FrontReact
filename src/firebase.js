895551785ef713583fdd9a8dc7b58c3484636aca
WellWellGood <wellwellgood@example.com>
2025-07-24 17:59:10 +0900

    File link firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,16 @@
-import admin from "firebase-admin";
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 
-const serviceAccount = {
-  "type": "service_account",
-  "project_id": "filefolder-54946",
-  "private_key_id": "YOUR_PRIVATE_KEY_ID",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nYOUR_KEY\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk@filefolder-54946.iam.gserviceaccount.com",
-  "client_id": "YOUR_CLIENT_ID",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk@filefolder-54946.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
+const firebaseConfig = {
+  apiKey: "AIzaSy...",
+  authDomain: "filefolder-54946.firebaseapp.com",
+  projectId: "filefolder-54946",
+  storageBucket: "filefolder-54946.appspot.com",
+  messagingSenderId: "1016654651914",
+  appId: "1:1016654651914:web:552cc88f977c5470d9b5f3"
 };
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: "filefolder-54946.appspot.com",
-  });
-}
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
 
-const bucket = admin.storage().bucket();
-export { bucket };
+export { storage };
---
+++ src/serverF/firebase.js
@@ -0,0 +1,25 @@
+import admin from "firebase-admin";
+
+const serviceAccount = {
+    "type": "service_account",
+    "project_id": "filefolder-54946",
+    "private_key_id": "YOUR_PRIVATE_KEY_ID",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nYOUR_KEY\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk@filefolder-54946.iam.gserviceaccount.com",
+    "client_id": "YOUR_CLIENT_ID",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk@filefolder-54946.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+};
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "filefolder-54946.appspot.com",
+  });
+}
+
+const bucket = admin.storage().bucket();
+export { bucket };

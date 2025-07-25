4478eed88ccf46504c7cc9a26f0186341fded983
WellWellGood <wellwellgood@example.com>
2025-07-25 13:44:34 +0900

    filebase key change

--- src/serverF/firebase.js
+++ src/serverF/firebase.js
@@ -3,19 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  token_uri: "https://oauth2.googleapis.com/token",
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 };
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  });
-}
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+});
 
 const bucket = admin.storage().bucket();
 export { bucket };

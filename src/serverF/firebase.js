4a5c2372a9a7d355fdc4dcef9bf2e89e9d7b920e
WellWellGood <wellwellgood@example.com>
2025-07-25 14:22:03 +0900

    filebase key change

--- src/serverF/firebase.js
+++ src/serverF/firebase.js
@@ -1,4 +1,5 @@
-import { initializeApp, cert } from "firebase-admin/app";
+// src/serverF/firebase.js
+import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getStorage } from "firebase-admin/storage";
 import dotenv from "dotenv";
 dotenv.config();
@@ -9,9 +10,14 @@ const firebaseConfig = {
   privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 };
 
-const app = initializeApp({
-  credential: cert(firebaseConfig),
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-});
+let app;
+if (!getApps().length) {
+  app = initializeApp({
+    credential: cert(firebaseConfig),
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  });
+} else {
+  app = getApps()[0]; // 이미 초기화된 앱 재사용
+}
 
 export const bucket = getStorage().bucket();

c9687f2699ed8654ca6b2b2ec0438cea6fbd0988
WellWellGood <wellwellgood@example.com>
2025-07-25 14:38:52 +0900

    filebass

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,14 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
+  apiKey: "AIzaSyCoRBViUbD7MSGC_2jxged-fBjGkOQC1So",
   authDomain: "filefolder-54946.firebaseapp.com",
   projectId: "filefolder-54946",
-  storageBucket: "filefolder-54946.appspot.com",
+  storageBucket: "filefolder-54946.appspot.com", // ← 수정됨
   messagingSenderId: "1016654651914",
-  appId: "1:1016654651914:web:552cc88f977c5470d9b5f3"
+  appId: "1:1016654651914:web:552cc88f977c5470d9b5f3",
+  measurementId: "G-HNXRF6QV2R"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];

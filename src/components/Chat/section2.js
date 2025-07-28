8ee540447455a5a73c1aa4bf8910e72f9a1622dd
WellWellGood <wellwellgood@example.com>
2025-07-28 10:00:40 +0900

    chat file upload

--- src/components/Chat/section2.js
+++ src/components/Chat/section2.js
@@ -198,7 +198,7 @@ const Section2 = () => {
         sender_username: username,
         receiver_username: selectedUser.username,
         receiver_name: selectedUser.name,
-        content: input.trim() || "[파일]",
+        // content: input.trim() || "[파일]",
         file_url: fileUrl,
         file_name: fileName,
         file_size: fileSize,
@@ -516,7 +516,7 @@ const Section2 = () => {
                               </button>
                             </div>
                           )}
-                          /*{convertTextToLink(msg.content || '내용 없음')}*/
+                          {convertTextToLink(msg.content || '내용 없음')}
                         </div>
                         <div className={styles.messageMeta}>
                           <span className={styles.time}>

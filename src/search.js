3abb5d53329fbdd2ad242a1485222a2be22dcacf
WellWellGood <wellwellgood@example.com>
2025-07-29 10:44:53 +0900

    ProFile Image

--- src/search.js
+++ src/search.js
@@ -25,6 +25,7 @@ const Search = () => {
 
   // ✅ 유저 정보 로드
   useEffect(() => {
+    const storedUsername = sessionStorage.getItem("username");
     const currentUser = sessionStorage.getItem("username") || 'defaultUser';
     const storedImage = sessionStorage.getItem(`profileImage_${currentUser}`);
   
